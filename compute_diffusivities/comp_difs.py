689ee73e621d253d41b71143a2f010145e36d5ec
Matheus-Novoa <novoa.matheus@gmail.com>
2020-07-31 16:40:17 -0300

    Refatorando código. Criando função ComputeCorrectedDiffusivity

--- compute_diffusivities/comp_difs.py
+++ compute_diffusivities/comp_difs.py
@@ -6,7 +6,27 @@ from sympy import diff
 from sympy.abc import P
 import json
 
+def ComputeCorrectedDiffusivity(gas_path):
+    dcm = {}
+    for root, _, files in os.walk(gas_path):
+        data = []
+        for name in files:
+            if name.endswith('txt'):
+                print(f'{root}{os.sep}{name}')
+                key = root.split(os.sep)[-2]
+                data.append(np.loadtxt(f'{root}{os.sep}{name}', usecols=[0, 1]))
 
+        if len(data) != 0:
+            data_array = np.asarray(data)
+            data_array[:, :, 1] **= 2
+            #index = np.where(data_array[0, :, 0] == 1000)[0][0]
+            fit_params = map(linregress, data_array[:, 4000:])
+            dc = [p.slope / 6 for p in fit_params]
+            dcm[float(key)] = np.mean(dc) * 1e-4 # A²/ps --> cm²/s
+
+    return dict(sorted(dcm.items()))
+
+        
 def compute_diff(gases, path):
     """
     Compute gases transport diffusion coefficient.
@@ -24,24 +44,8 @@ def compute_diff(gases, path):
     gas_dict = None
 
     for gas in gases:
-        dcm = {}
-        for root, _, files in os.walk(gas):
-            data = []
-            for name in files:
-                if name.endswith('txt'):
-                    # print(f'{root}{os.sep}{name}')
-                    key = root.split(os.sep)[-1][:-3]
-                    data.append(np.loadtxt(f'{root}{os.sep}{name}', usecols=[0, 1]))
-
-            if len(data) != 0:
-                data_array = np.asarray(data)
-                data_array[:, :, 1] **= 2
-                index = np.where(data_array[0, :, 0] == 1000)[0][0]
-                fit_params = map(linregress, data_array[:, index:])
-                dc = [p.slope / 6 for p in fit_params]
-                dcm[float(key)] = round(np.mean(dc) * 1e-4, 6)
-
-        dcm = dict(sorted(dcm.items()))
+        
+        dc_dict = ComputeCorrectedDiffusivity(gas)
 
         gas_name = gas.split(os.sep)[-1]
 
@@ -54,7 +58,7 @@ def compute_diff(gases, path):
         if gas_dict is None:
             gas_dict = {}
 
-        gas_dict[gas_name] = pd.DataFrame(list(dcm.values()), index=dcm.keys(), columns=['D0 (cm^2/s)'])
+        gas_dict[gas_name] = pd.DataFrame(list(dc_dict.values()), index=dc_dict.keys(), columns=['D0 (cm^2/s)'])
         gas_dict[gas_name]['Termodyamic Factor'] = [round(float(C / Press / Iso_diff.subs(P, Press)), 2) for Press, C in
                                                     zip(Iso_data['P [bar]'], Iso_data[gas_name])]
         gas_dict[gas_name]['Dt (cm^2/s)'] = round(
